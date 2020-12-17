9077975b58d2d6d4126fab77249fe64b97f258af
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-17 12:06:38 +0500

    Update ProductItem.jsx

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import Shoes from '../../shoes.json';
 
 export const ProductItem = () => {
+
+    const { id } = useParams();
+    const shoe = Shoes[id];
+    if (!shoe) {
+        return <h1>Product Not Found!</h1>
+    }
+    console.log("Shoe =>", shoe);
+
     return (
         <div>
             <h1>Product Item</h1>
+            <div>
+                <img src={shoe.img} alt={shoe} height={250} />
+                <h2>{shoe.name}</h2>
+            </div>
         </div>
     )
 }
