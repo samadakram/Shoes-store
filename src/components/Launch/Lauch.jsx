0f227efd60fc8c9ab5a63d6e90860909acf1a6a3
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-17 10:53:22 +0500

    Create Lauch.jsx

---
+++ src/components/Launch/Lauch.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Shoes from '../../shoes.json';
+import { Link } from 'react-router-dom';
+
+export const Products = () => {
+
+    console.log("Shoes =>", Shoes);
+    return (
+        <div>
+            <h1>Products</h1>
+            {Object.keys(Shoes).map((keyName) => {
+                const shoe = Shoes[keyName];
+                return (<Link to={`products/${keyName}`}>
+                    <img src={shoe.img} alt={shoe} height={250} />
+                    <h2 key={keyName}>{shoe.name}</h2>
+                </Link>)
+            })}
+        </div>
+    )
+}
