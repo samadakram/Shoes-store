5107f1e3b859627bdbdcdf3cae7400d84616388e
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-17 11:08:54 +0500

    Update Lauch.jsx

--- src/components/Launch/Lauch.jsx
+++ src/components/Launch/Lauch.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Shoes from '../../shoes.json';
 import { Link } from 'react-router-dom';
+import { Grid } from '@material-ui/core';
 
 export const Launch = () => {
 
@@ -8,13 +9,19 @@ export const Launch = () => {
     return (
         <div>
             <h1>Products</h1>
-            {Object.keys(Shoes).map((keyName) => {
-                const shoe = Shoes[keyName];
-                return (<Link to={`products/${keyName}`}>
-                    <img src={shoe.img} alt={shoe} height={250} />
-                    <h2 key={keyName}>{shoe.name}</h2>
-                </Link>)
-            })}
+            <Grid container>
+                {Object.keys(Shoes).map((item, ind) => {
+                    const shoe = Shoes[item];
+                    return (
+                        <Grid item md={4}>
+                            <Link to={`products/${item}`}>
+                                <img src={shoe.img} alt={shoe} height={250} />
+                                <h2 key={ind}>{shoe.name}</h2>
+                            </Link>
+                        </Grid>
+                    )
+                })}
+            </Grid>
         </div>
     )
 }
