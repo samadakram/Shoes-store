d09ced44ac15587d06abd4095fcb8a1d77f5209d
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-17 10:17:44 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Home } from './components/Home';
+import { Products } from './components/Products';
+import { NavBar } from './components/NavBar/NavBar';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Router>
+      <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="products" element={<Products />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
