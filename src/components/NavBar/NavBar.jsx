6f4f036612e38fa2f9ea2cfcb55a74c58eec2dfc
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-17 10:20:59 +0500

    Update NavBar.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import styles from './NavBar.module.css';
+
+
 import { Link } from 'react-router-dom';
 
 export const NavBar = () => {
     return (
-        <div>
+        <div className={styles.container}>
             <Link to="/">Home</Link>{(' || ')}
             <Link to="products">Products</Link>
         </div>
