5ed4ed3def9413d385d6f58fa9a9fad8ec253d0e
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-18 15:24:31 +0500

    Update ProductItem.jsx

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Shoes from '../../shoes.json';
 import { Grid, Typography, InputLabel, FormControl, NativeSelect } from '@material-ui/core';
@@ -6,7 +6,7 @@ import styles from './Item.module.css';
 
 export const ProductItem = () => {
 
-    const [state, setState] = useState({
+  const [state, setState] = useState({
     age: '',
     name: 'hai',
   }); // Material Ui
@@ -19,65 +19,65 @@ export const ProductItem = () => {
     });
   };
 
-    const { id } = useParams();
-    const shoe = Shoes[id];
-    if (!shoe) {
-        return <h1>Product Not Found!</h1>
-    }
-    console.log("Shoe =>", shoe);
+  const { id } = useParams();
+  const shoe = Shoes[id];
+  if (!shoe) {
+    return <h1>Product Not Found!</h1>
+  }
+  console.log("Shoe =>", shoe);
 
-    return (
-        <div>
-            <h1>Product Item</h1>
-            <Grid container>
-                <Grid item xs={12} md={6} className={styles.grid}>
-                    <div>
-                        <img src={shoe.img} alt={shoe} width={"100%"} height={400} />
-                    </div>
-                </Grid>
-                <Grid item xs={12} md={6} className={styles.grid}>
-                    <div>
-                        <Typography variant="h5">
-                            <h2>{shoe.name}</h2>
+  return (
+    <div>
+      <h1>Product Item</h1>
+      <Grid container>
+        <Grid item xs={12} md={6} className={styles.grid}>
+          <div>
+            <img src={shoe.img} alt={shoe} width={"100%"} height={400} />
+          </div>
+        </Grid>
+        <Grid item xs={12} md={6} className={styles.grid}>
+          <div>
+            <Typography variant="h5">
+              <h2>{shoe.name}</h2>
+            </Typography>
+            <Typography variant="subtitle2" gutterBottom>
+              BRAND: Studio Footwear
                         </Typography>
-                        <Typography variant="subtitle2" gutterBottom>
-                            BRAND: Studio Footwear
+            <Typography variant="subtitle2" gutterBottom>
+              Product Code: geet-fone-320009
                         </Typography>
-                        <Typography variant="subtitle2" gutterBottom>
-                            Product Code: geet-fone-320009
+            <Typography variant="subtitle2" gutterBottom>
+              Availability: In Stock
                         </Typography>
-                        <Typography variant="subtitle2" gutterBottom>
-                            Availability: In Stock
+            <Typography variant="h6" gutterBottom>
+              Available Options
                         </Typography>
-                        <Typography variant="h6" gutterBottom>
-                            Available Options
-                        </Typography>
-                        <FormControl className={styles.formControl}>
-        <InputLabel shrink htmlFor="age-native-label-placeholder">
-          Size
+            <FormControl className={styles.formControl}>
+              <InputLabel shrink htmlFor="age-native-label-placeholder">
+                Size
         </InputLabel>
-        <NativeSelect
-          className={styles.select}
-          value={state.age}
-          onChange={handleChange}
-          inputProps={{
-            name: 'age',
-            id: 'age-native-label-placeholder',
-          }}
-        >
-          <option value="">---Please Select---</option>
-          <option value={6}>PK 6 - EU 36</option>
-          <option value={7}>PK 7 - EU 37</option>
-          <option value={8}>PK 8 - EU 38</option>
-          <option value={9}>PK 9 - EU 39</option>
-          <option value={10}>PK 10 - EU 40</option>
-          <option value={11}>PK 11 - EU 41</option>
-          <option value={12}>PK 12 - EU 42</option>
-        </NativeSelect>
-      </FormControl>
-                    </div>
-                </Grid>
-            </Grid>
-        </div>
-    )
+              <NativeSelect
+                className={styles.select}
+                value={state.age}
+                onChange={handleChange}
+                inputProps={{
+                  name: 'age',
+                  id: 'age-native-label-placeholder',
+                }}
+              >
+                <option value="">---Please Select---</option>
+                <option value={6}>PK 6 - EU 36</option>
+                <option value={7}>PK 7 - EU 37</option>
+                <option value={8}>PK 8 - EU 38</option>
+                <option value={9}>PK 9 - EU 39</option>
+                <option value={10}>PK 10 - EU 40</option>
+                <option value={11}>PK 11 - EU 41</option>
+                <option value={12}>PK 12 - EU 42</option>
+              </NativeSelect>
+            </FormControl>
+          </div>
+        </Grid>
+      </Grid>
+    </div>
+  )
 }
