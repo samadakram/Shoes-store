edbcd353144c74076f947506cf38a9590706e1be
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-18 16:36:23 +0500

    Update NavBar.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,10 +9,10 @@ export const NavBar = () => {
     return (
         <div className={styles.container}>
             <Grid container>
-                <Grid item md={6} className={styles.gridItem}>
+                <Grid item xs={12} md={6} className={styles.gridItem}>
                     <Typography variant="h5" color="primary">SHOE STORE</Typography>
                 </Grid>
-                <Grid item md={6} className={styles.gridItem}>
+                <Grid item xs={12} md={6} className={styles.gridItem}>
                     <Link to="/">Home</Link>{(' || ')}
                     <Link to="launch">Launch</Link>
                 </Grid>
