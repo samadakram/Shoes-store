aae9ec5614706fbad15a31aa4a356adbc3955943
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-12-18 16:30:40 +0500

    Update NavBar.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './NavBar.module.css';
+import { Grid, Typography } from '@material-ui/core';
 
 
 import { Link } from 'react-router-dom';
@@ -7,8 +8,15 @@ import { Link } from 'react-router-dom';
 export const NavBar = () => {
     return (
         <div className={styles.container}>
-            <Link to="/">Home</Link>{(' || ')}
-            <Link to="launch">Launch</Link>
+            <Grid container>
+                <Grid item md={6}>
+                    <Typography variant="h5" color="primary">SHOE STORE</Typography>
+                </Grid>
+                <Grid item md={6}>
+                    <Link to="/">Home</Link>{(' || ')}
+                    <Link to="launch">Launch</Link>
+                </Grid>
+            </Grid>
         </div>
     )
 }
